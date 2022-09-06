50923b3631f5aec0367c509cf763fc854bb85a80
Maulik <nirmal.kanani@yahoo.com>
2022-09-06 09:47:05 +0530

    Update Sidebar

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,15 +1,39 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import Sidebar from '../View/Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import { Admin_Final } from '../Routes'
 import AdminNav from '../AdminView/AdminNav'
 
+
 const Admin = () => {
+
+    const [toggle, settoggle] = useState(true)
+
     return (
         <div className='container-fluid'>
             <div className="row">
-                <div className="col-md-2 border-end p-0 d-none d-md-block">
-                    <Sidebar />
+                <div className="col-12">
+                    <div className="d-flex">
+                        <div style={{ width: `${toggle ? '10%' : '20%'}` }}>
+                            <Sidebar toggle={toggle} settoggle={settoggle} />
+                        </div>
+                        <div style={{
+                            width: `${toggle ? '90%' : '80%'}`
+                        }}>
+                            <Suspense fallback={<div><h4>Loading</h4></div>}>
+                                <Routes>
+                                    {
+                                        Admin_Final.map((element, index) =>
+                                            <Route path={element.path} key={index} element={<element.component />} />
+                                        )
+                                    }
+                                </Routes>
+                            </Suspense>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="col-md-2 border-end p-0 d-none d-md-block">
+                    <Sidebar toggle={toggle} settoggle={settoggle}/>
                 </div>
                 <div className="col-md-10 p-0">
                     <div className="row">
@@ -26,7 +50,7 @@ const Admin = () => {
                             }
                         </Routes>
                     </Suspense>
-                </div>
+                </div> */}
             </div>
         </div>
     )
--- src/View/Sidebar/Sidebar.js
+++ src/View/Sidebar/Sidebar.js
@@ -1,28 +1,94 @@
-import Divider from '@mui/material/Divider';
+import React, { useEffect, useState } from 'react';
+import "./_sidebar.scss";
+import { MdGpsNotFixed, MdOutlineTrackChanges } from "react-icons/md"
+import { BiChevronRight } from "react-icons/bi"
+import { AiFillCaretDown } from "react-icons/ai"
+import { FaRegLightbulb } from "react-icons/fa"
+import { FcInTransit } from "react-icons/fc"
 import { Admin_Route } from '../../Navigation/Nav';
-import React from 'react'
-import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+
+const Sidebar = ({ toggle, settoggle }) => {
+
+    const handleToggle = () => {
+        settoggle(!toggle)
+    }
+
+    function toggleOnScreenResize() {
+        if (window.screen.width <= 768) {
+            settoggle(!toggle)
+        } else {
+            settoggle(toggle)
+        }
+    }
+    useEffect(() => {
+        toggleOnScreenResize();
+        window.addEventListener('resize', () => {
+            toggleOnScreenResize();
+        })
+
+        return () => {
+            window.removeEventListener('resize', () => { })
+        }
+    }, [])
+
     return (
-        <div className='sidebar ps-md-3 '>
-            <div className="row">
-                <div className="sara-logo p-2 col-12">
-                    <img src="/assets/images/web_black.png" alt="SARA LOGO" height={50} />
+        <nav className={`s_sidebar ${toggle ? `s_close` : ''}`}>
+            <header className='overflow-hidden'>
+                <div className="s_image-text">
+                    <div className="s_logo-text">
+                        <span className="s_name">AI Optimizer</span>
+                    </div>
                 </div>
-                <div className="col-12">
-                    <ul className='list-unstyled'>
+                <BiChevronRight className='s_toggle' onClick={handleToggle} />
+
+                <div className='s_project_name-wrapper'>
+                    <FcInTransit className='s_icon' />
+                    <span className="s_text project_name">Pixis AI</span>
+                </div>
+            </header>
+
+            <div className="s_menu-bar">
+                <div className="s_menu">
+                    <ul className="s_menu-links">
                         {
-                            Admin_Route.map((element, index) =>
-                                <Link to={element.url} key={index} className="text-start">
-                                    <li className='p-3 fs-6 text-dark'><i className={element.icon} /><span className='ms-3'>{element.name}</span></li>
-                                </Link>
+                            Admin_Route.map((element, index) => 
+                            <li className="s_nav-link">
+                            <a href="#">
+                                <span className='s_icon'><i className={element.icon}></i></span>
+                                <span className="s_text nav-text">{element.name}</span>
+                                <AiFillCaretDown className='s_icon-right' />
+                            </a>
+                        </li>
                             )
                         }
+                        {/* <li className="s_nav-link">
+                            <a href="#">
+                                <MdGpsNotFixed className='s_icon' />
+                                <span className="s_text nav-text">Governance AI</span>
+                                <AiFillCaretDown className='s_icon-right' />
+                            </a>
+                        </li>
+                        <li className="s_nav-link">
+                            <a href="#">
+                                <MdOutlineTrackChanges className='s_icon' />
+                                <span className="s_text nav-text">Targeting AI</span>
+                                <AiFillCaretDown className='s_icon-right' />
+                            </a>
+                        </li>
+                        <li className="s_nav-link">
+                            <a href="#">
+                                <FaRegLightbulb className='s_icon' />
+                                <span className="s_text nav-text">Creative AI</span>
+                                <AiFillCaretDown className='s_icon-right' />
+                            </a>
+                        </li> */}
+
                     </ul>
                 </div>
             </div>
-        </div>
+
+        </nav>
     )
 }
 
